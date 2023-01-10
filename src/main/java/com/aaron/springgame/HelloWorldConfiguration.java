52f0d12e866298f158b11b05e340ce892ceb110a
Aaron Spence <aaronspence31@gmail.com>
2023-01-10 18:55:10 -0500

    configuring spring beans

--- src/main/java/com/aaron/springgame/HelloWorldConfiguration.java
+++ src/main/java/com/aaron/springgame/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.aaron.springgame;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) { };
+record Person(String name, int age, Address address) { };
 record Address(String firstLine, String city) { };
 
 @Configuration
@@ -21,11 +21,26 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("Joe", 20);
+        return new Person("Joe", 19, new Address("Main Street", "Oakville"));
     }
 
     @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) {
+        return new Person(name, age, address3);
+    }
+
+    @Bean(name = "address2")
     public Address address() {
         return new Address("Yonge Street", "Toronto");
+    }
+
+    @Bean(name = "address3")
+    public Address address3() {
+        return new Address("King Street", "Waterloo");
     }
 }
