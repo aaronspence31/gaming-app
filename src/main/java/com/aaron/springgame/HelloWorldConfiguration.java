6a5c3941b6e9bef310390426af54548b28e34916
Aaron Spence <aaronspence31@gmail.com>
2023-01-10 18:37:36 -0500

    add more beans

--- src/main/java/com/aaron/springgame/HelloWorldConfiguration.java
+++ src/main/java/com/aaron/springgame/HelloWorldConfiguration.java
@@ -3,11 +3,29 @@ package com.aaron.springgame;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age) { };
+record Address(String firstLine, String city) { };
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name() {
         return "Aaron";
+    }
+
+    @Bean
+    public int age() {
+        return 20;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("Joe", 20);
+    }
+
+    @Bean
+    public Address address() {
+        return new Address("Yonge Street", "Toronto");
     }
 }
