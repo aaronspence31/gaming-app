6c04516f6edf9330b1d8ae9bae0e603f1d40463a
Aaron Spence <aaronspence31@gmail.com>
2023-01-10 18:29:52 -0500

    hello world

---
+++ src/main/java/com/aaron/springgame/App02HelloWorldSpring.java
@@ -0,0 +1,16 @@
+package com.aaron.springgame;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class App02HelloWorldSpring {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+        System.out.println(context.getBean("name"));
+    }
+
+}
